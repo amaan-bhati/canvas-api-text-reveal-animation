97982882a116805d418a5a7cbdf23aa978fb892c
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-06-21 23:43:44 +0530

    initial commit

---
+++ src/components/Text.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Text() {
+  return (
+    <div className="absolute flex flex-col text-[5.5vw] uppercase w-[80vw] items-start leading-tight">
+      <p>Text reveal animation</p>
+      <p className="self-end">Gives a sense of scratch on hovering</p>
+      <p className='text-[2.3vw] capitalize'>
+      Contact <a href='https://amaanbhatitech'>@amaanbhati.tech</a> for source code</p>
+      
+    </div>
+  )
+}
